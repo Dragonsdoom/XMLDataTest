a414a7499925e22dfed92fe334970806014c0b9c
Frank V <frank@protodragon.com>
2012-09-10 21:45:05 -0400

    Initial Commit

---
+++ DatabaseController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+namespace XMLDataTest
+{
+    public class DatabaseController
+    {
+        string connect = "Server = localhost; Database = cs153db; Uid = root; Pwd = \"\";";
+        MySqlConnection con;
+        MySqlCommand command;
+
+        public DatabaseController()
+        {
+            con = new MySqlConnection(connect);
+            command = new MySqlCommand();
+            command.Connection = con;
+            command.CommandText = "SELECT * FROM ORDERS";
+        }
+
+        public void SetTable(string tableName)
+        {
+            command.CommandText = "SELECT * FROM " + tableName;
+        }
+
+        public MySqlDataReader GetReader()
+        {
+            MySqlDataReader reader;
+            reader = command.ExecuteReader();
+            return reader;
+        }
+
+        public bool TryConnectToDatabase()
+        {
+            //Try open connection
+            try
+            {
+                con.Open();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return false;
+            }
+#if DEBUG
+            Console.WriteLine("Made the connection to the MySQL database.");
+#endif
+            return true;
+        }
+
+        public bool TryCloseConnection()
+        {
+            try
+            {
+                if (con.State != System.Data.ConnectionState.Closed)
+                {
+                    con.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Xml;
+using MySql.Data.MySqlClient;
+
+namespace XMLDataTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseController dc = new DatabaseController();
+            
+            if(!dc.TryConnectToDatabase()) 
+                return;
+
+            if (args.Length > 0)
+            {
+                dc.SetTable(args[0]);
+            }
+            else
+            {
+                dc.SetTable("ITEM");
+            }
+            DumpTableToXML(dc, "output");
+
+            if (!dc.TryCloseConnection())
+                return;
+        }
+
+        /// <summary>
+        /// Using the provided database information, this method dumps the current table to an xml file.
+        /// </summary>
+        /// <param name="dc">the database we use</param>
+        /// <param name="filename">the name of the file with no extension</param>
+        private static void DumpTableToXML(DatabaseController dc, string filename)
+        {
+            DataTable dataTable;
+            
+            using (MySqlDataReader reader = dc.GetReader())
+            {
+                dataTable = reader.GetSchemaTable();
+                dataTable.Load(reader);
+            }
+            XmlTextWriter xmlTWriter = new XmlTextWriter(Directory.GetCurrentDirectory() + "\\" + filename + ".xml", Encoding.Default);
+            xmlTWriter.Formatting = Formatting.Indented;
+            try
+            {
+                dataTable.WriteXml(xmlTWriter, true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
